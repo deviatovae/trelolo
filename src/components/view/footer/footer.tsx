573d15d2fcbe67bf2a3358d853dbbf7475bc6c2f
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-02-01 23:35:27 +0300

    feat: add noreferrer params

--- src/components/view/footer/footer.tsx
+++ src/components/view/footer/footer.tsx
@@ -6,16 +6,16 @@ const Footer = () => {
   return (
     <footer className="footer">
         <div className="footer__dev">
-          <a className="github-link" href="https://github.com/deviatovae" target="_blank">deviatovae</a>
+          <a className="github-link" href="https://github.com/deviatovae" target="_blank" rel="noreferrer">deviatovae</a>
         </div>
         <div className="footer__dev">
-          <a className="github-link" href="https://github.com/AlexeiKozlovskiy" target="_blank">alexeikozlovskiy</a>
+          <a className="github-link" href="https://github.com/AlexeiKozlovskiy" target="_blank" rel="noreferrer">alexeikozlovskiy</a>
         </div>
         <div className="footer__dev">
-          <a className="github-link" href="https://github.com/kira-zaytseva" target="_blank">kira-zaytseva</a>
+          <a className="github-link" href="https://github.com/kira-zaytseva" target="_blank" rel="noreferrer">kira-zaytseva</a>
         </div>
       <div className="footer__school">
-        <a className="footer__school-link" href="https://rs.school/js/" target="_blank">
+        <a className="footer__school-link" href="https://rs.school/js/" target="_blank" rel="noreferrer">
           <p className="footer__school-img"></p>
         </a>
       </div>
