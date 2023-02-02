73c4f15e131dfdf14d8e9275e464140bb9c95036
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-02-02 23:20:26 +0300

    feat: add logo redirect to welcome page

--- src/components/view/header/header.tsx
+++ src/components/view/header/header.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './headerViews.scss';
 
+
+
 const Header = () => {
+  const history = useNavigate();
+
+  const handleLogoClick = () => {
+    history('/');
+  };
+
   return (
     <header className="header">
-      <div className="header__logo-wrapper">
+      <div className="header__logo-wrapper" onClick={handleLogoClick}>
         <div className="header__logo"></div>
         <div className="header__logo-text">trelolo</div>
       </div>
-
       <div className="header__lang-user-wrapper">
         <div className="header__lang">EN</div>
         <div className="header__user">KZ</div>
