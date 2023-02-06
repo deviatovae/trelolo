3f3d3c535884138c6a8b4dfaa1f699a71ec7e91b
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-02-07 00:13:25 +0300

    refactor: changes styles

--- src/components/aside/aside.tsx
+++ src/components/aside/aside.tsx
@@ -6,16 +6,21 @@ export const Aside = () => {
   return (
     <aside className="aside__container">
         <div className="aside__tasks-team">
-          <div className="tasks-team">My tasks</div>
-          <div className="tasks-team">My team</div>
+          {/* <div className="tasks-team">My tasks</div> */}
+          <div className="tasks-team">
+            <div className="tasks-team__logo"></div>
+            My team
+          </div>
         </div>
         <div className="aside__projects">
-          <div className="projects__my-projects">My-projects
+          <div className="projects__my-projects">
+          <span className="projects__logo"></span>
+            My-projects
             <span className="projects__plus">+</span>
           </div>
           <ul className="projects__list">
-            <li className="projects__item">trelolo</li>
-            <li className="projects__item">my project</li>
+            <li className="projects__item"><span>trelolo</span></li>
+            <li className="projects__item"><span>my project</span></li>
           </ul>
         </div>
     </aside>
