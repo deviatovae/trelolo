a77afc254a2b7a934d1607b61815113892c19eb3
Alena Deviatova <aedeviatova@yandex.ru>
2023-02-11 22:20:51 +0300

    fix: projects api service

--- src/API/projectService.ts
+++ src/API/projectService.ts
@@ -6,6 +6,6 @@ export const getProjects = () => getResponse<List<Project>>({ url: 'projects', m
 
 export const createProject = (project: ProjectData) => getResponse<Project>({ url: 'projects', method: RequestMethod.POST, body: project });
 
-export const updateProject = (project: ProjectData, id: number) => getResponse<Project>({ url: `projects/:${id}`, method: RequestMethod.PATCH, body: project });
+export const updateProject = (id: string, project: ProjectData) => getResponse<Project>({ url: `projects/${id}`, method: RequestMethod.PATCH, body: project });
 
-export const deleteProject = (id: number) => getResponse<void>({ url: `projects/${id}`, method: RequestMethod.DELETE });
+export const deleteProject = (id: string) => getResponse<void>({ url: `projects/${id}`, method: RequestMethod.DELETE });
