4b2e7c5de30e7d718f3d9e16d37acd7f7d5c7336
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-02-18 12:24:28 +0300

    feat: add profile page layout

--- src/pages/profilePage/profilePage.tsx
+++ src/pages/profilePage/profilePage.tsx
@@ -11,39 +11,40 @@ export const ProfilePage = () => {
   return (
     <main className="profilePage__wrapper">
         <Aside />
-        <section className="profilePage__section">
+        <section className="profilePage__section-wrapper">
+          <div className="profilePage__section">
             <div className="profilePage__header">Profile settings</div>
             <div className="profilePage__content-wrapper">
-              <div className="profilePage__content-person">
+                <div className="profilePage__person-wrapper">
                 <ProfilePerson id={userInfo?.id ?? ''} name={userInfo?.name ?? ''} email={userInfo?.email ?? ''}/>
-                <div className="modal-windows__left-right-container">
-                    <div className="modal-windows__left">
-                      <div className="modal-windows__name-input-container">
-                        <div className="modal-windows__name-header">Your full name</div>
-                        <input type="text" className="modal-windows__input-name"></input>
+                </div>
+                <div className="profilePage__left-right-container">
+                    <div className="profilePage__left-side">
+                      <div className="profilePage__name-input-container">
+                        <div className="profilePage__name-header">Your full name</div>
+                        <input type="text" value={userInfo?.name} className="profilePage__input-name profilePage-inputs"></input>
                       </div>
-                      <div className="modal-windows__color-container">
-                        <div className="modal-windows__color-header">Pick color</div>
-                        <input type="color" className="modal-windows__palitra"></input>
+                      <div className="profilePage__color-container">
+                        <div className="profilePage__color-header">Pick color</div>
+                        <input type="color" className="profilePage__palitra"></input>
                       </div>
-                      <div className="modal-windows__email-container">
-                        <div className="modal-windows__email-header">Email address</div>
-                        <input type="text" className="modal-windows__email"></input>
+                      <div className="profilePage__email-container">
+                        <div className="profilePage__email-header">Email address</div>
+                        <input type="text" value={userInfo?.email} className="profilePage__email profilePage-inputs"></input>
                       </div>
                     </div>
-                    <div className="modal-windows__right">
-                     <div className="modal-windows__pasword-container">
-                       <div className="modal-windows__pasword-header">Change password</div>
-                       <input type="pasword" className="modal-windows__pasword"></input>
-                       <input type="pasword" className="modal-windows__pasword"></input>
-                       <input type="pasword" className="modal-windows__pasword"></input>
+                    <div className="profilePage__right-side">
+                    <div className="profilePage__pasword-header">Change password</div>
+                     <div className="profilePage__pasword-container">
+                       <input type="pasword" placeholder="Enter current password" className="profilePage__pasword profilePage-inputs"></input>
+                       <input type="pasword" placeholder="Enter new password" className="profilePage__pasword profilePage-inputs"></input>
+                       <input type="pasword" placeholder="Confirm password" className="profilePage__pasword profilePage-inputs"></input>
                      </div>
-                    <div className="modal-windows__save-brn">Save changes</div> 
+                    <button className="profilePage__save-btn">Save changes</button> 
                   </div>
                 </div>
-              </div>
             </div>
-        
+          </div>
         </section>
     </main>
   );
