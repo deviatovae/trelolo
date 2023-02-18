678d2889a1976c4d99b8cccf288e2e6abbbddcb8
Alena Deviatova <aedeviatova@yandex.ru>
2023-02-18 11:32:50 +0300

    fix: moving to first position

--- src/context/tasksContext.tsx
+++ src/context/tasksContext.tsx
@@ -169,7 +169,7 @@ export const TasksProvider = ({ children }: { children: ReactNode }) => {
         };
       });
 
-      const { errors } = await TaskService.moveTask(taskId, toSectionId, toIndex);
+      const { errors } = await TaskService.moveTask(taskId, toSectionId, (toIndex || 0) + 1);
       return errors;
     } catch (e) {
       return castToErrors(e);
