87470a1d388c9bcbbab28cc9df2261a1ffb64782
Kira Zaytseva <kira.zaytseva97@gmail.com>
2023-02-19 23:35:14 +0000

    feat: add dynamic user icon and remove year in the task

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -4,13 +4,18 @@ import { Task as TaskModel } from '../../types/models';
 import { useTaskModal } from '../../hooks/useTaskModal';
 import { Draggable } from 'react-beautiful-dnd';
 import { useTasks } from '../../hooks/useTasks';
-import { formatDate } from '../../utils/formatDate';
+import { formatDateString } from '../../utils/formatDate';
 import './task.scss';
+import { useMembers } from '../../hooks/members';
 
 export const Task = ({ task, index }: { task: TaskModel, index: number }) => {
   const context = useTasks();
   const { showTaskModal } = useTaskModal();
-  const { name } = task;
+  const { name, assignees } = task;
+  const { getGroupedMembers } = useMembers();
+  const members = getGroupedMembers();
+
+  const assignee = members.find((member) => member.id === assignees[0]?.memberId);
 
   return (
     <Draggable key={task.id} draggableId={task.id} index={index}>
@@ -24,12 +29,12 @@ export const Task = ({ task, index }: { task: TaskModel, index: number }) => {
         >
           <div className="task-title-logo-container">
             <p className="task-title">{name}</p>
-            <UserIcon userId="24">KZ</UserIcon>
+            { assignee && <UserIcon userId={assignee.id}>{assignee.name}</UserIcon>}
           </div>
           <div className="task-calendar-settings-container">
             <div className="task-deadline">
               <div className="task-calendar"></div>
-              {task.dueDate && <span className="task-deadline">{formatDate(new Date(task.dueDate), 'PP')}</span>}
+              {task.dueDate && <span className="task-deadline">{formatDateString(task.dueDate, 'd MMMM')}</span>}
             </div>
             <div className="task-settings"></div>
           </div>
