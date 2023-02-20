cb523db72662638929aaf573837e6eb6f41fb525
Alena Deviatova <aedeviatova@yandex.ru>
2023-02-20 18:34:20 +0300

    fix: remove extra sections contexts

--- src/pages/membersPage/membersPage.tsx
+++ src/pages/membersPage/membersPage.tsx
@@ -1,19 +1,14 @@
 import './membersPage.scss';
 import { MembersProvider } from '../../context/membersContext';
-import { ProjectsProvider } from '../../context/projectsContext';
 import { MemberPageContent } from './memberPageContent';
 
 export const MembersPage = () => {
 
   return (
-    <>
-      <ProjectsProvider>
-        <div className="members-page__content">
-          <MembersProvider>
-            <MemberPageContent />
-          </MembersProvider>
-        </div>
-      </ProjectsProvider>
-    </>
+    <div className="members-page__content">
+      <MembersProvider>
+        <MemberPageContent />
+      </MembersProvider>
+    </div>
   );
 };
