c2fdf97dc4b9d810e10721c47675a876f879bcc2
Alena Deviatova <aedeviatova@yandex.ru>
2023-02-26 10:42:41 +0300

    feat: pass accept language header to backend

--- src/components/languages/languagesManager.ts
+++ src/components/languages/languagesManager.ts
@@ -1,23 +1,24 @@
 import { useState } from 'react';
 import { LOCALES } from './locales';
 import { enUS, ru } from 'date-fns/locale';
+import { Locale } from '../../utils/locale';
 
+function getInitialLocal() {
+  const savedLocale = localStorage.getItem('locale');
+  return savedLocale || LOCALES.ENGLISH;
+}
+
+const fnsLocales = {
+  'ru-RU': ru,
+  'en-US': enUS,
+};
 
 export const LanguagesManager = () => {
   const [currentLocale, setCurrentLocale] = useState(getInitialLocal());
+  Locale.currentLocale = currentLocale;
   const setLocale = (locale: string) => {
     setCurrentLocale(locale);
     localStorage.setItem('locale', locale);
-  };
-
-  function getInitialLocal() {
-    const savedLocale = localStorage.getItem('locale');
-    return savedLocale || LOCALES.ENGLISH;
-  }
-
-  const fnsLocales = {
-    'ru-RU': ru,
-    'en-US': enUS,
   };
 
   const fnsLocale = fnsLocales[currentLocale as keyof typeof fnsLocales] || enUS;
