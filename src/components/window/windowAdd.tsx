e78744a964616691277deff90355a9b48229be8e
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-02-27 22:45:07 +0300

    fix: changes length limits input

--- src/components/window/windowAdd.tsx
+++ src/components/window/windowAdd.tsx
@@ -24,7 +24,7 @@ export const WindowAdd = ({ onCreate, placeholderProps }: ModalProps) => {
           placeholder={placeholderProps}
           onChange={(event) => setInputValue(event.target.value)} 
           onKeyDown={CheckKeyDown}
-          maxLength={29}
+          maxLength={25}
           >
         </input>
         <button className="modal-main__btn-create-project" disabled={!inputValue.length} onClick={handleCreateProject}>{trans(Message.Create)}</button>
